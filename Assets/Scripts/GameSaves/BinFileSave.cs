4373f5bf52d0fd0ba239ae425fa03a42d1292338
Filip Toruń <filip.torun@mousetrap.games>
2020-11-14 21:20:16 +0100

    added saving bin file, renamed -> saltedSave to caesarCypherSave

---
+++ Assets/Scripts/GameSaves/BinFileSave.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using static FileSave;
+
+public class BinFileSave : GameSave {
+    protected SimpleIntSave save;
+    /// <summary>
+    /// Setup save type
+    /// </summary>
+    public virtual void Awake(){
+        save = new SimpleIntSave(0);
+    }
+
+    public override void Save() {
+        string path = $"{Application.persistentDataPath}/{saveName}.bin";
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(path, FileMode.Create);
+        save.value = value;
+        bf.Serialize(stream, save.GetJson());
+        stream.Close();
+        Debug.LogError($"Saved: {path}");
+    }
+    public override void Load() {
+        string path = $"{Application.persistentDataPath}/{saveName}.bin";
+        if (System.IO.File.Exists(path)) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            try {
+                save.FromJson(bf.Deserialize(stream) as string);
+            } catch (System.Runtime.Serialization.SerializationException) {
+                stream.Close();
+                return;
+            }
+            stream.Close();
+            value = save.value;
+        }
+    }
+}
--- Assets/Scripts/GameSaves/FileSaveCaesarCipher.cs
+++ Assets/Scripts/GameSaves/FileSaveCaesarCipher.cs
@@ -3,27 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FileSaveWithSalt : FileSave {
+public class FileSaveCaesarCipher : FileSave {
     /// <summary>
     /// Setup save type
     /// </summary>
     public override void Awake() {
-        save = new SaltedIntSave(0);
+        save = new CaesarCipherIntSave(0);
     }
 
     [Serializable]
-    public class SaltedIntSave : SimpleIntSave {
-        private int GetSalt { get => 13377331; }
-        public SaltedIntSave(int newValue) {
+    public class CaesarCipherIntSave : SimpleIntSave {
+        private int GetShift { get => 13377331; }
+        public CaesarCipherIntSave(int newValue) {
             value = newValue;
         }
         public override string GetJson() {
-            SaltedIntSave tempSave = new SaltedIntSave(value + GetSalt);
+            CaesarCipherIntSave tempSave = new CaesarCipherIntSave(value + GetShift);
             return JsonUtility.ToJson(tempSave);
         }
         public override void FromJson(string json) {
             JsonUtility.FromJsonOverwrite(json, this);
-            value -= GetSalt;
+            value -= GetShift;
         }
     }
 }
